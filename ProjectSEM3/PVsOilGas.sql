b53ad895c0c731c5c8b982c3ef4b2fa3b8840504
Phuong <vxp20xx@gmail.com>
2023-06-06 20:50:32 +0700

    Update UI Hr

--- ProjectSEM3/PVsOilGas.sql
+++ ProjectSEM3/PVsOilGas.sql
@@ -47,21 +47,20 @@ insert into Hr(Name,Email,Password) values('Phuong','phuong@gmail.com','111111')
 	IdLevel int references Level(Id),
 	Content ntext default '',
 	Point int default 1,
-	A ntext default '',
-	B ntext default '',
-	C ntext default '',
-	D ntext default '',
-	CorrectAnwser varchar(10) default '',
+	Options nvarchar(max) default '',
+	CorrectAnwser varchar(10),
 	IsMultiAnwser bit default 1,
 	Status bit default  1
 )
+select * from Type 
+select * from level 
 
 select * from Question for json path
 insert into Question(IdType,IdLevel,Content,Point,A,B,C,D,CorrectAnwser,IsMultiAnwser) values
-(1,1,'Question content 1',4,'Question Anwser A1','Question Anwser B1','Question Anwser C1','Question Anwser D1','A',0),
-(1,1,'Question content 2',3,'Question Anwser A2','Question Anwser B2','Question Anwser C2','Question Anwser D2','D',0)
+(1,1,'Question content 1',4,'[{'A' : 'ádasdasdasdasd'},{'B' : 'ádasdasdasdasd'},{'C' : 'ádasdasdasdasd'},{'D' : 'ádasdasdasdasd'}]','A',0),
+(1,1,'Question content 2',3,'[{'A' : 'ádasdasdasdasd'},{'B' : 'ádasdasdasdasd'},{'C' : 'ádasdasdasdasd'},{'D' : 'ádasdasdasdasd'}]','D',0)
 
-create table ContestantQuestion(
+drop table ContestantQuestion(
 	Id int identity(1,1) primary key,
 	IdQuestion int references Question(Id),
 	IdContestant int references Contestant(Id),
@@ -69,4 +68,4 @@ create table ContestantQuestion(
 	Status bit default 1
 )
 
-select * from Hr for json path
+select * from type for json path
