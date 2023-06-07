1c7b53b9696fede475d374e372671f69daabbaec
Pham Van <85114119+phongvan-1412@users.noreply.github.com>
2023-06-08 04:39:08 +0700

    -DB: Create model CV + Job

---
+++ ProjectSEM3/Models/Entities/CV.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectSEM3.Models.Entities
+{
+    public class CV
+    {
+        public class Req
+        {
+            public int Id { get; set; }
+            public int Job { get; set; }
+            public string Name { get; set; }
+            public string Email { get; set; }
+            public HttpPostedFileBase File { get; set; }
+            public bool Status { get; set; }
+            public Req() { }
+        }
+
+        public class Res
+        {
+            public int Id { get; set; }
+            public int Job { get; set; }
+            public string Name { get; set; }
+            public string Email { get; set; }
+            public HttpPostedFileBase File { get; set; }
+            public bool Status { get; set; }
+            public Res() { }
+        }
+    }
+}
---
+++ ProjectSEM3/Models/Entities/Job.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectSEM3.Models.Entities
+{
+    public class Job
+    {
+        public class Req
+        {
+            public int Id { get; set; }
+            public string Title { get; set; }
+            public string Location { get; set; }
+            public string DatePosted { get; set; }
+            public string EmpType { get; set; }
+            public string Content { get; set; }
+            public bool Status { get; set; }
+            public Req()
+            {
+
+            }
+        }
+        public class Res
+        {
+            public int Id { get; set; }
+            public string Title { get; set; }
+            public string Location { get; set; }
+            public string DatePosted { get; set; }
+            public string EmpType { get; set; }
+            public string Content { get; set; }
+            public bool Status { get; set; }
+            public Res()
+            {
+
+            }
+        }
+    }
+}
