e68806127ed22edcb7df9d1136221ba803290d4d
Pham Van <85114119+phongvan-1412@users.noreply.github.com>
2023-06-20 12:13:51 +0700

    -Client (Quiz): Update UI -Client (Career): Change route

--- ProjectSEM3/Controllers/CareerController.cs
+++ ProjectSEM3/Controllers/CareerController.cs
@@ -17,7 +17,7 @@ namespace ProjectSEM3.Controllers
         {
             return View();
         }
-        public ActionResult Jobs()
+        public ActionResult Jobs(string lstCountries)
         {
             return View();
         }
@@ -51,5 +51,10 @@ namespace ProjectSEM3.Controllers
             return RedirectToAction("Index");
         }
 
+        [HttpPost]
+        public ActionResult Search(string lstCountries)
+        {
+            return RedirectToAction("Jobs", lstCountries);
+        }
     }
 }
