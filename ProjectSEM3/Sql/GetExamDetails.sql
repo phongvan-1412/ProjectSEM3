341e3621ebadb96054f10fa0edee56e30dd74f14
Phuong <vxp20xx@gmail.com>
2023-06-25 08:59:24 +0700

    Update

--- ProjectSEM3/Sql/GetExamDetails.sql
+++ ProjectSEM3/Sql/GetExamDetails.sql
@@ -2,7 +2,7 @@ alter function GetExamDetails() returns table
 as
 	return
 		(
-			select ed.Id,ed.ExamId,ed.Answer,ed.[Status],
+			select ed.QuestionId,ed.ExamId,ed.Answer,ed.[Status],
 					q.Content,q.Point,q.Options,q.CorrectAnwser,
 					q.IsMultiAnwser,q.TypeId,qt.[Name] as TypeName,
 					q.LevelId,l.[Name] as LevelName,
