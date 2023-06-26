9ac0a678bce376cdd29c3bd4e413760000b481d9
Pham Van <85114119+phongvan-1412@users.noreply.github.com>
2023-06-27 01:52:02 +0700

    -Client (Quiz): Handle Point

--- ProjectSEM3/Controllers/QuizController.cs
+++ ProjectSEM3/Controllers/QuizController.cs
@@ -52,10 +52,127 @@ namespace ProjectSEM3.Controllers
 
 
         [HttpPost]
-        public JsonResult Submit(string result, int contestantId)
+        public JsonResult Submit(ContestantExam result, int contestantId)
         {
-            var json = JsonConvert.DeserializeObject(result);
-            List<Result> results = JsonConvert.DeserializeObject<List<Result>>(json.ToString());
+            var math = result.Math;
+            var knowledge = result.Knowledge;
+            var computer = result.Computer;
+            List<string> answerKnowledge;
+            List<string> correctAnswerKnowledge;
+            int pointMath = 0;
+            int pointKnowledge = 0;
+            int pointComputer = 0;
+            int totalPoint = 0;
+
+            foreach (var item in knowledge)
+            {
+                bool checkKnowledge = false;
+                int checkMulti = 0;
+
+                if(item.TypeId != 3)
+                {
+                    if(item.TypeId == 2)
+                    {
+                        if (item.Answer.Equals(item.CorrectAnwser))
+                        {
+                            checkKnowledge = true;
+                        }
+                        if (checkKnowledge)
+                        {
+                            pointKnowledge += item.Point;
+                        }
+                    }
+                    else
+                    {
+                        answerKnowledge = JsonConvert.DeserializeObject<List<string>>(item.Answer);
+                        correctAnswerKnowledge = JsonConvert.DeserializeObject<List<string>>(item.CorrectAnwser);
+
+                        foreach (var ele in answerKnowledge)
+                        {
+                            foreach (var correctEle in correctAnswerKnowledge)
+                            {
+                                if (item.TypeId == 1)
+                                {
+                                    if (!item.IsMultiAnwser)
+                                    {
+                                        if (ele.Equals(correctEle))
+                                        {
+                                            checkKnowledge = true;
+                                        }
+                                    }
+                                    else
+                                    {
+                                        if (ele.Equals(correctEle))
+                                        {
+                                            checkMulti += 1;
+                                        }
+                                    }
+                                    if (checkKnowledge || checkMulti == correctAnswerKnowledge.Count())
+                                    {
+                                        pointKnowledge += item.Point;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            foreach (var item in math)
+            {
+                bool checkMath = false;
+                int checkMulti = 0;
+                List<string> answerMath = JsonConvert.DeserializeObject<List<string>>(item.Answer);
+                List<string> correctAnswerMath = JsonConvert.DeserializeObject<List<string>>(item.CorrectAnwser);
+
+                foreach (var ele in answerMath)
+                {
+                    foreach (var correctEle in correctAnswerMath)
+                    {
+                        if (item.TypeId == 1)
+                        {
+                            if (!item.IsMultiAnwser)
+                            {
+                                if (ele.Equals(correctEle))
+                                {
+                                    checkMath = true;
+                                }
+                            }
+                            else
+                            {
+                                if (ele.Equals(correctEle))
+                                {
+                                    checkMulti += 1;
+                                }
+                            }
+                            if (checkMath || checkMulti==correctAnswerMath.Count())
+                            {
+                                pointMath += item.Point;
+                            }
+                        }
+                        else if(item.TypeId == 3)
+                        {
+                            if (item.Answer.Equals(item.CorrectAnwser))
+                            {
+                                checkMath = true;
+                                pointMath += item.Point;
+                            }
+                        }
+                    }
+                }
+            }
+            
+            foreach (var item in computer)
+            {
+                bool checkComputer = false;
+                List<string> answerComputer = JsonConvert.DeserializeObject<List<string>>(item.Answer);
+                List<string> correctAnswerComputer = JsonConvert.DeserializeObject<List<string>>(item.CorrectAnwser);
+                //if (item.Answer.Equals(item.CorrectAnwser))
+                //{
+                //    checkComputer = true;
+                //    pointComputer += item.Point;
+                //}
+            }
+
             return Json(new { redirectToUrl = Url.Action("Home", "Index") });
         }
 
